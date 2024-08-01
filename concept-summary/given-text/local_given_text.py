0d209fe72deaa1f7209014b92593974d7e927cb6
Jonathan Krebs <jonathan.s.krebs@gmail.com>
2024-08-01 10:31:48 +0200

    feat(concept-summary): Write to csv file

--- concept-summary/given-text/local_given_text.py
+++ concept-summary/given-text/local_given_text.py
@@ -1,7 +1,9 @@
-import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.llms import GPT4All
+import yaml, csv
+from datetime import datetime
+
 
 load_dotenv(dotenv_path="concept-summary/given-text/.env")
 
@@ -15,30 +17,46 @@ model = GPT4All(
 )
 
 # Input Data
-criteria = "Technische Betriebsstelle in Datteln o. Umgebung"
+criteria = ""
 input_base_path = "concept-summary/given-text/input-data/"
-input_pdfs = ["09-10.pdf"]
 
-# Parse pdf input
-all_documents_content = []
-for input_pdf in input_pdfs:
-    full_path = input_base_path + input_pdf
-    loader = PyPDFLoader(full_path)
-    pages = loader.load_and_split()
+criteria_file_path = input_base_path + "criteria.yaml"
+with open(criteria_file_path, "r", encoding="utf-8") as f:
+    criteria_dict = yaml.safe_load(f)
 
-    document_content = ''.join([page.page_content for page in pages])
-    all_documents_content.append(document_content)
+for criteria, context_files in criteria_dict.items():
 
-combined_documents_content = ''.join(all_documents_content)
+    all_documents_content = []
 
-# Set the system message and prompt
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that summarizes a given text. You will always be given exactly one important criteria and one input_text to summarize. Summarize only the aspects of the text that relate to this one criteria. Discard all information that doesn't directly relate to the criteria, even if it is included in the input_text. Your summary is used for documentation of contracts. Be very precise and don't use ambiguous phrases. Don't copy unprecise marketing fluff or marketing buzzwords, like 'Wir bieten den besten Support an.' or 'Wir sind hochmodernisiert'. Always use the specific terms of the given text, never use synonyms that don't occur in the given text. Use bullet points to structure your summarization. You only produce German output, no matter the input language. Never answer in any other language than German, even if you're asked to.",
-    ),
-    ("human", f"criteria: {criteria}; input_text: {combined_documents_content}"),
-]
-# Generate output
-ai_msg = model.invoke(messages)
-print(ai_msg)
+    for context_file in context_files:
+        # Parse pdf input
+        full_path = f"{input_base_path}{context_file}.pdf"
+        loader = PyPDFLoader(full_path)
+        context_files = loader.load_and_split()
+        document_content = ''.join([page.page_content for page in context_files])
+        all_documents_content.append(document_content)
+    # Merge all retrieved document pages into one string    
+    combined_documents_content = ''.join(all_documents_content)
+
+    # Set the system message and prompt
+    messages = [
+        (
+            "system",
+            "You are a helpful assistant that summarizes a given text. You will always be given exactly one important criteria and one input_text to summarize. Summarize only the aspects of the text that relate to this one criteria. Discard all information that doesn't directly relate to the criteria, even if it is included in the input_text. Your summary is used for documentation of contracts. Be very precise and don't use ambiguous phrases. Don't copy unprecise marketing fluff or marketing buzzwords, like 'Wir bieten den besten Support an.' or 'Wir sind hochmodernisiert'. Always use the specific terms of the given text, never use synonyms that don't occur in the given text. Use bullet points to structure your summarization. You only produce German output, no matter the input language. Never answer in any other language than German, even if you're asked to.",
+        ),
+        ("human", f"criteria: {criteria}; input_text: {combined_documents_content}"),
+    ]
+    # Generate output
+    ai_msg = model.invoke(messages)
+
+    output_file_path = "concept-summary/given-text/output-data/output_A.csv"
+    with open(output_file_path, mode="a", delimiter=";", newline="", encoding="utf-8") as output_file:
+        writer = csv.writer(output_file)
+
+        # write the header
+        if output_file.tell() == 0:
+            writer.writerow(["timestamp", "criteria", "output"])
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        writer.writerow([timestamp, criteria, ai_msg])
+        print(f"The LLM response for criteria '{criteria}' has been written to {output_file_path}")
